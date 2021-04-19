31d7d14264263b71f188f3e6422562866a2a17c8
Pablo <ccx54392@gmail.com>
2021-04-18 19:47:26 -0600

    added constar for website url

--- src/main/java/io/corecode/testing/MyConstants.java
+++ src/main/java/io/corecode/testing/MyConstants.java
@@ -3,5 +3,7 @@ package io.corecode.testing;
 public class MyConstants {
     public static final String url="http://myapi-env.eba-3me5caza.us-east-2.elasticbeanstalk.com/";
 
+    public static final String website="http://mywebsite2-env.eba-yihsxqmu.us-east-2.elasticbeanstalk.com/";
+
     //public static final String url="http://localhost:8080/myapi/";
 }
