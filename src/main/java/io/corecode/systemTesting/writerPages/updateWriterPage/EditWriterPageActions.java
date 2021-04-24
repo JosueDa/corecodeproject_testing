b086dd7030d6879729fc898874a09c9f364109be
Pablo <ccx54392@gmail.com>
2021-04-23 22:40:22 -0600

    waits changed to use the wait variable

--- src/main/java/io/corecode/systemTesting/writerPages/updateWriterPage/EditWriterPageActions.java
+++ src/main/java/io/corecode/systemTesting/writerPages/updateWriterPage/EditWriterPageActions.java
@@ -16,16 +16,19 @@ public class EditWriterPageActions extends EditWriterPageElements{
         PageFactory.initElements(driver,this);
     }
     public String getMessage(){
-        new WebDriverWait(driver,5).until(ExpectedConditions.presenceOfElementLocated(By.id("message")));
+        //new WebDriverWait(driver,5).until(ExpectedConditions.presenceOfElementLocated(By.id("message")));
+        wait.until(ExpectedConditions.visibilityOf(this.message));
         return this.message.getText();
     }
     public void enterName(String name){
-        new WebDriverWait(driver,5).until(ExpectedConditions.presenceOfElementLocated(By.id("name")));
+        //new WebDriverWait(driver,5).until(ExpectedConditions.presenceOfElementLocated(By.id("name")));
+        wait.until(ExpectedConditions.visibilityOf(this.name));
         JavascriptExecutor j = (JavascriptExecutor)driver;
         j.executeScript("arguments[0].value='"+name+"';", this.name);
     }
     public void submit(){
-        new WebDriverWait(driver,5).until(ExpectedConditions.presenceOfElementLocated(By.id("submitUpdateWriter")));
+        //new WebDriverWait(driver,5).until(ExpectedConditions.presenceOfElementLocated(By.id("submitUpdateWriter")));
+        wait.until(ExpectedConditions.visibilityOf(this.submit));
         JavascriptExecutor j = (JavascriptExecutor)driver;
         j.executeScript("arguments[0].click();", this.submit);
     }
