93e607c73db1272dd606274cf6aff764dc07f6dd
Pablo <ccx54392@gmail.com>
2021-04-25 22:08:56 -0600

    added information so static variables can get information from the surefire parameters

--- src/main/java/io/corecode/MyConstants.java
+++ src/main/java/io/corecode/MyConstants.java
@@ -4,15 +4,26 @@ public class MyConstants {
     /**
      * api
      */
-    public static final String url="http://myapi-env.eba-3me5caza.us-east-2.elasticbeanstalk.com/";
-    //public static final String url="http://localhost:8080/myapi/";
-    //public static final String url="http://localhost:8080/";
+    public static String url="http://myapi-env.eba-3me5caza.us-east-2.elasticbeanstalk.com/";
+    //public static String url="http://localhost:8080/myapi/";
+    //public static String url="http://localhost:8080/";
 
 
     /**
      * website
      */
-    public static final String website="http://mywebsite2-env.eba-yihsxqmu.us-east-2.elasticbeanstalk.com/";
-    //public static final String website="http://localhost:8080/";
+    public static String website="http://mywebsite2-env.eba-yihsxqmu.us-east-2.elasticbeanstalk.com/";
+    //public static String website="http://localhost:8080/";
 
+    static {
+        String webUrl=System.getProperty("webUrl");
+        if(webUrl!=null && !webUrl.equals("")){
+            website=webUrl;
+        }
+        String apiUrl=System.getProperty("apiUrl");
+        if(apiUrl!=null && !apiUrl.equals("")){
+            url=apiUrl;
+        }
+
+    }
 }
