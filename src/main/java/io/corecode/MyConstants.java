1831dd47a6a6a7ff1df2fba19c7088c4bb5a3a2e
Pablo <ccx54392@gmail.com>
2021-04-25 18:52:50 -0600

    commented a line

--- src/main/java/io/corecode/MyConstants.java
+++ src/main/java/io/corecode/MyConstants.java
@@ -6,6 +6,8 @@ public class MyConstants {
      */
     public static final String url="http://myapi-env.eba-3me5caza.us-east-2.elasticbeanstalk.com/";
     //public static final String url="http://localhost:8080/myapi/";
+    //public static final String url="http://localhost:8080/";
+
 
     /**
      * website
