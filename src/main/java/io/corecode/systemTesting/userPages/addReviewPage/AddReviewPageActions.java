e60054baf5180818af5b7d54bea3bb7ffc5a043b
Pablo <ccx54392@gmail.com>
2021-04-29 23:12:08 -0600

    added wait

--- src/main/java/io/corecode/systemTesting/userPages/addReviewPage/AddReviewPageActions.java
+++ src/main/java/io/corecode/systemTesting/userPages/addReviewPage/AddReviewPageActions.java
@@ -22,6 +22,7 @@ public class AddReviewPageActions extends AddReviewPageElements{
     }
 
     public void enterReviewDescription(String description){
+        wait.until(ExpectedConditions.visibilityOf(this.reviewDescription));
         this.reviewDescription.sendKeys(description);
     }
 
