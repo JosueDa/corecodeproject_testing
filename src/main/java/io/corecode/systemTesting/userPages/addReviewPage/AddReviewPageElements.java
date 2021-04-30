206086f987c416bd5979bad496d5a26e90a46c63
Pablo <ccx54392@gmail.com>
2021-04-29 23:29:55 -0600

    added wait

--- src/main/java/io/corecode/systemTesting/userPages/addReviewPage/AddReviewPageElements.java
+++ src/main/java/io/corecode/systemTesting/userPages/addReviewPage/AddReviewPageElements.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import java.util.List;
 
 public class AddReviewPageElements {
-    @FindBy(id="enterDescription")
+    @FindBy(xpath = "//*[@id='enterDescription']")
     protected WebElement reviewDescription;
     @FindBy(id="submitReview")
     protected WebElement submitReview;
