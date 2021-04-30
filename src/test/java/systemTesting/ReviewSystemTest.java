f2c4f390394cda181c390dfd733590ec9e1a7d43
Pablo <ccx54392@gmail.com>
2021-04-29 23:08:31 -0600

    removed unnecesary prints

--- src/test/java/systemTesting/ReviewSystemTest.java
+++ src/test/java/systemTesting/ReviewSystemTest.java
@@ -49,13 +49,10 @@ public class ReviewSystemTest extends InitDriver {
     public void updateReviewBookPage(){
         userPage = new LogInAsUser(driver,wait).logRegularUser("user","user");
         userPage.clickFirstBookLink();
-        System.out.println("[DEBUG] clicked on first book");
         addReview = new AddReviewPageActions(driver,wait);
         addReview.clickLastUpdateLink();
-        System.out.println("[DEBUG] clicked on update link of last review");
         updateReview = new UpdateReviewPageActions(driver,wait);
         String message = updateReview.updateReview(3,"test qa update 0");
-        System.out.println("[DEBUG] enter stars, description and get message");
         Assert.assertEquals(message,"Review was updated successfully");
         System.out.println("[TEST PASSED] "+message);
     }
